208820f37d02c9ff8f141e81d1cbc3fc617aac07
Filipe Plucenio <plucenio@gmail.com>
2024-02-13 21:44:33 -0300

    implementing unit tests for the datasources.

--- test/data/datasources/open_weather_map_datasource_test.dart
+++ test/data/datasources/open_weather_map_datasource_test.dart
@@ -21,8 +21,11 @@ void main() {
         'should return CurrentWeatherResponse when the call to API is successful',
         () async {
           // Arrange
-          //final Map<String, dynamic> queryParameters = {'q': 'London'};
+          const int id = 1;
+          const int visibility = 2;
           final Map<String, dynamic> expectedResponse = {
+            'id': id,
+            'visibility': visibility,
             'weather': [
               {
                 'id': 800,
@@ -51,6 +54,53 @@ void main() {
 
           // Assert
           expect(result, isA<CurrentWeatherResponseModel>());
+          expect(result.id, id);
+          expect(result.visibility, visibility);
+          verify(() => mockHttpClient.get(any(),
+              queryParameters: any(named: 'queryParameters'),
+              options: any(named: 'options'))).called(1);
+        },
+      );
+
+      test(
+        'should return ForecastWeatherResponse when the call to API is successful',
+        () async {
+          // Arrange
+          const String cod = '1';
+          const int message = 1;
+          final Map<String, dynamic> expectedResponse = {
+            'cod': cod,
+            'message': message,
+            'weather': [
+              {
+                'id': 800,
+                'main': 'Clear',
+                'description': 'clear sky',
+                'icon': '01d'
+              }
+            ],
+          };
+          when(
+            () => mockHttpClient.get(
+              any(),
+              queryParameters: any(named: 'queryParameters'),
+              options: any(named: 'options'),
+            ),
+          ).thenAnswer(
+            (final invocation) async => Response(
+              data: expectedResponse,
+              requestOptions: RequestOptions(),
+            ),
+          );
+
+          // Act
+          final result = await datasource.getForecastWeatherByLocation(
+              location: LocationModel());
+
+          // Assert
+          expect(result, isA<ForecastWeatherResponseModel>());
+          expect(result.cod, cod);
+          expect(result.message, message);
           verify(() => mockHttpClient.get(any(),
               queryParameters: any(named: 'queryParameters'),
               options: any(named: 'options'))).called(1);
