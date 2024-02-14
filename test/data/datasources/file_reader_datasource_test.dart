b4c256126020398c43c7dbcf4dfaf415b55c8bd5
Filipe Plucenio <plucenio@gmail.com>
2024-02-14 19:35:12 -0300

    implemented some file reader tests

--- test/data/datasources/file_reader_datasource_test.dart
+++ test/data/datasources/file_reader_datasource_test.dart
@@ -20,7 +20,7 @@ void main() {
         'should return an empty list when the call to API is empty',
         () async {
           // Arrange
-          const String expectedResponse = "{}";
+          const String expectedResponse = '{}';
           when(
             () => mockAssetBundle.loadString(
               key: any(named: 'key'),
@@ -41,7 +41,7 @@ void main() {
       );
 
       test(
-        'should return an empty list when the call to API is empty',
+        'should return a list with one city',
         () async {
           // Arrange
           const String expectedResponse =
@@ -60,15 +60,39 @@ void main() {
           // Assert
           expect(result, isA<List<CityModel>>());
           expect(
-            result,
-            <CityModel>[
-              CityModel(
-                name: 'Silverston',
-                country: 'United Kingdom',
-                lat: '52.0738',
-                lng: '-1.0168',
-              )
-            ],
+            result.length,
+            1,
+          );
+          verify(() => mockAssetBundle.loadString(key: any(named: 'key')))
+              .called(1);
+        },
+      );
+
+      test(
+        'should return a list with more than one city',
+        () async {
+          // Arrange
+          const String expectedResponse = '{"data":['
+              '{"name": "Silverston","country": "United Kingdom","lat": "52.0738","lng": "-1.0168"},'
+              '{"name": "São Paulo","country": "Brazil","lat": "10","lng": "10"},'
+              '{"name": "Torres","country": "Brazil","lat": "10","lng": "10"}'
+              ']}';
+          when(
+            () => mockAssetBundle.loadString(
+              key: any(named: 'key'),
+            ),
+          ).thenAnswer(
+            (final invocation) async => expectedResponse,
+          );
+
+          // Act
+          final result = await datasource.readCities();
+
+          // Assert
+          expect(result, isA<List<CityModel>>());
+          expect(
+            result.length > 1,
+            true,
           );
           verify(() => mockAssetBundle.loadString(key: any(named: 'key')))
               .called(1);
