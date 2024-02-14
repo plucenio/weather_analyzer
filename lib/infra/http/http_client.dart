09433e71c0476bf221e6b4b6a6cea77b09dbef49
Filipe Plucenio <plucenio@gmail.com>
2024-02-14 14:41:22 -0300

    improvements on Dio adapter documentation.

--- lib/infra/http/http_client.dart
+++ lib/infra/http/http_client.dart
@@ -2,6 +2,17 @@ import 'package:dio/dio.dart';
 
 import 'http.dart';
 
+/// HttpClient adapter for Dio.
+///
+/// This adapter is used to interface with the Dio package to make HTTP requests.
+/// It provides a concrete implementation of the IHttpClient interface for Dio,
+/// allowing for easy swapping of HTTP clients if needed.
+///
+/// Example usage:
+/// ```dart
+/// final httpClient = HttpClient(dio: Dio());
+/// final response = await httpClient.get<String>('https://api.example.com/data');
+///
 class HttpClient implements IHttpClient {
   final Dio dio;
 
